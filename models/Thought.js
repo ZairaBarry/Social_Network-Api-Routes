c3964a388932d62b97b014ca7bece2995dc89dad
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-03-13 23:22:17 -0300

    created thought module

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, model, Types } = require('mangoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => dateFormat(date)
+    },
+});
+
+const thoughScehma = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => dateFormat(date)
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    recations: [reactionSchema]
+}
+  
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+
+    }
+;
