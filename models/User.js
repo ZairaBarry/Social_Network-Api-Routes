0aef31a8ef71d563a11eb854725d6933d14917d1
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-03-13 22:30:19 -0300

    created user module

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            max_length: 50,
+            unique: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+//virtual friendCount
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User;
